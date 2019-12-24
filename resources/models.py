204d6731315751f402b4f313e3aadd1e0e9aea4a
Ludovic Houdayer <ludovic.houdayer@fr.clara.net>
2019-12-24 17:09:35 +0100

    py-lint bug?
    Signed-off-by: Ludovic Houdayer <ludovic.houdayer@fr.clara.net>

--- resources/models.py
+++ resources/models.py
@@ -4,13 +4,13 @@ from run import db
 class UserModel(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)                            # pylint: disable=no-member
+    username = db.Column(db.String(120), unique=True, nullable=False)       # pylint: disable=no-member
+    password = db.Column(db.String(120), nullable=False)                    # pylint: disable=no-member
 
     def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+        db.session.add(self)                                                # pylint: disable=no-member
+        db.session.commit()                                                 # pylint: disable=no-member
 
     @classmethod
     def find_by_username(cls, username):
@@ -45,12 +45,12 @@ class UserModel(db.Model):
 
 class RevokedTokenModel(db.Model):
     __tablename__ = 'revoked_tokens'
-    id = db.Column(db.Integer, primary_key=True)
-    jti = db.Column(db.String(120))
+    id = db.Column(db.Integer, primary_key=True)                            # pylint: disable=no-member
+    jti = db.Column(db.String(120))                                         # pylint: disable=no-member
 
     def add(self):
-        db.session.add(self)
-        db.session.commit()
+        db.session.add(self)                                                # pylint: disable=no-member
+        db.session.commit()                                                 # pylint: disable=no-member
 
     @classmethod
     def is_jti_blacklisted(cls, jti):
