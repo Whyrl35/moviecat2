8b29e064878561dff90ab8ae1513c0b489943d07
Ludovic Houdayer <ludovic.houdayer@fr.clara.net>
2019-12-27 18:26:36 +0100

    initialize the bootstrapvue + vuejs with a sample

---
+++ www/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <Listcard/>
+  </div>
+</template>
+<script>
+ import Listcard from './components/Listcard.vue'
+  import Navbar from './components/Navbar.vue';
+  export default {
+    name: 'navbar',
+    components: {
+      Navbar,
+      Listcard
+    }
+  }
+</script>
