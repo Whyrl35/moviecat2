f6eb5f466ab19e5101164dd5f1b0758ff8ae1e96
Ludovic Houdayer <ludovic.houdayer@fr.clara.net>
2020-01-09 10:47:41 +0100

    put the right api
    Signed-off-by: Ludovic Houdayer <ludovic.houdayer@fr.clara.net>

--- www/src/components/Cards.vue
+++ www/src/components/Cards.vue
@@ -51,7 +51,7 @@ export default {
   },
   mounted() {
     axios
-      .get("http://localhost:5000/v1/movies")
+      .get("http://moviecat.ks.whyrl.fr/api/v1/movies")
       .then(response => {
         this.movies = response.data.data.movies;
       })
