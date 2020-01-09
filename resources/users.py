aef1d4e1255d9283e0e6dfabc96d9c41e5be4f72
Ludovic Houdayer <ludovic.houdayer@fr.clara.net>
2020-01-09 09:16:48 +0100

    fix datetime import
    Signed-off-by: Ludovic Houdayer <ludovic.houdayer@fr.clara.net>

--- resources/users.py
+++ resources/users.py
@@ -4,7 +4,7 @@ from flask_restful import Resource, reqparse
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
 from run import app, api
 from .models import UserModel, RevokedTokenModel
-from datetime import datetime
+import datetime
 
 
 parser = reqparse.RequestParser()
